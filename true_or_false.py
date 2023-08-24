257084ad6a936858ce04c99fe078e7efd8b85194
Ransford Oduro <ransfordtakyi149@gmail.com>
2023-08-24 12:39:36 +0100

    This is my final commit message. I used a function called true_or_false that accept two arguments : one for the string input and the second for the letter. I then used the for loop and the if condition to return true or false when a condition is met

--- true_or_false.py
+++ true_or_false.py
@@ -1,4 +1,12 @@
 
 def true_or_false(string_input,letter):
     for char in string_input:
-        if char
+        if char == letter:
+            return "true"
+        else:
+            return "false"
+
+string_input = input("enter a string")
+letter = input("enter a letter to search from the string")
+
+print(true_or_false(string_input,letter))
